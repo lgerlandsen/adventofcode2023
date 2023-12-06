b72dfc14b7a9a7ebb34af9dbe927bfc5e0845432
Leif Gunnar Erlandsen <leif@lako.no>
2023-12-06 13:14:16 +0100

    Update a.js

--- day1/a.js
+++ day1/a.js
@@ -1,9 +1,9 @@
-import { open } from 'node:fs/promises';
+//import { open } from 'node:fs/promises';
 myFileReader();
 const re1 = /(\d)/
 const re2 = /(\d)*?$/
 async function myFileReader() {
-    const file = await open('day1/testinput1a.txt');
+    const file = await fs.open('day1/testinput1a.txt');
     for await (const line of file.readLines()) {
         console.log(line)
         siffer1 = re1.line;
