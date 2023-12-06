51060ae705720c75262b67471ca2f13bf0ada4e0
Leif Gunnar Erlandsen <leif@lako.no>
2023-12-06 13:12:27 +0100

    Update a.js

--- day1/a.js
+++ day1/a.js
@@ -1,6 +1,13 @@
-var fs = require('fs'), filename = 'day1/testinput1a.txt';
-fs.readFile(filename, 'utf8', function(err, data) {
-  if (err) throw err;
-  console.log('OK: ' + filename);
-  console.log(data)
-});
+import { open } from 'node:fs/promises';
+myFileReader();
+const re1 = /(\d)/
+const re2 = /(\d)*?$/
+async function myFileReader() {
+    const file = await open('day1/testinput1a.txt');
+    for await (const line of file.readLines()) {
+        console.log(line)
+        siffer1 = re1.line;
+        siffer2 = re2.line;
+        console.log('siffer1=', siffer1, ' siffer2=', siffer2);
+    }
+}
