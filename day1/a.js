ca8b884c7826564b1deac7d8d7ab23c8e1941189
Leif Gunnar Erlandsen <leif@lako.no>
2023-12-06 13:36:52 +0100

    Update a.js

--- day1/a.js
+++ day1/a.js
@@ -12,7 +12,7 @@ var  lineReader = readLine.createInterface({
 });
 lineReader.on("line", function (line) {
         console.log(line)
-        siffer1 = line.re1;
-        siffer2 = line.re2;
+        siffer1 = line.match(re1);
+        siffer2 = line.match(re2);
         console.log('siffer1=', siffer1, ' siffer2=', siffer2);
 });
