e74bae7cd54aa20a6179a48f1bcf7c741a3f0780
Leif Gunnar Erlandsen <leif@lako.no>
2023-12-06 15:15:38 +0100

    Update b.js

--- day1/b.js
+++ day1/b.js
@@ -1,20 +1,20 @@
 const { open } = require('node:fs/promises');
 
-const re1 = /(\d)/
-const re2 = /.*(\d).*?$/
+const re1 = /(one|two|three|four|five|six|seven|eight|nine|\d)/
+const re2 = /.*(one|two|three|four|five|six|seven|eight|nine|\d).*?$/
 
 let sum = 0;
-let siffer1 = 0;
-let siffer2 = 0;
+var siffer1 ;
+var siffer2 ;
 ( async () => {
-    const file = await open('day1/input1a.txt');
+    const file = await open('day1/testinput1b.txt');
     for await ( const line of file.readLines()) {
         //console.log(line)
-        siffer1 = Number(line.match(re1)[1]);
-        siffer2 = Number(line.match(re2)[1]);
-        //console.log('siffer1=', siffer1, ' siffer2=', siffer2);
-        sum += siffer1 * 10 + siffer2 
+        siffer1 = line.match(re1)[1];
+        siffer2 = line.match(re2)[1];
+        console.log('siffer1=', siffer1, ' siffer2=', siffer2);
+        //sum += siffer1 * 10 + siffer2 
     }
-console.log('Day 1 part a');
-console.log('Sum=', sum);
+console.log('Day 1 part b');
+//console.log('Sum=', sum);
 })();
