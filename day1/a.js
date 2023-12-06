485ea3ea9ace6dd05e03f923f1ec0f213fcf88ad
Leif Gunnar Erlandsen <leif@lako.no>
2023-12-06 12:32:40 +0100

    Create a.js

---
+++ day1/a.js
@@ -0,0 +1,6 @@
+var fs = require('fs'), filename = 'testinput1a.txt';
+fs.readFile(filename, 'utf8', function(err, data) {
+  if (err) throw err;
+  console.log('OK: ' + filename);
+  console.log(data)
+});
