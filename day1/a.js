b79b38d0b7adf6562a496d54ae0c01bcc25be31b
Leif Gunnar Erlandsen <leif@lako.no>
2023-12-06 13:19:05 +0100

    Update a.js

--- day1/a.js
+++ day1/a.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 const re1 = /(\d)/
 const re2 = /(\d)*?$/
 const file = fs.open('day1/testinput1a.txt');
-    for await (const line of file.readLines()) {
+    for (const line of file.readLines()) {
         console.log(line)
         siffer1 = re1.line;
         siffer2 = re2.line;
